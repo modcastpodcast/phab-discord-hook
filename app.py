7251aa7cc41f9e243adab70cf545334c7112f7fa
Joseph Banks <joseph@josephbanks.me>
2020-07-25 00:16:00 +0100

    First attempt at webhooking task creations

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from os import environ
 
 from flask import Flask, request
@@ -6,13 +7,57 @@ import httpx
 app = Flask(__name__)
 
 WEBHOOK_URL = environ.get("WEBHOOK_URL")
+API_TOKEN = environ.get("API_TOKEN")
+API_BASE = environ.get("API_BASE")
 
+
+def handle_task(data):
+    task_transactions = httpx.post(f"{API_BASE}/transaction.search", json={
+        "api.token": API_TOKEN,
+        "objectIdentifier": data["object"]["phid"]
+    }).json()
+
+    new_transactions = [t["phid"] for t in task_transactions["data"] if t["type"] == "create"]
+    hook_transactions = [t["phid"] for t in data["transactions"]]
+
+    task_data = httpx.post(f"{API_BASE}/maniphest.query", json={
+        "api.token": API_TOKEN,
+        "phids": [
+            data["object"]["phid"]
+        ]
+    }).json()[data["object"]["phid"]]
+
+    author_data = httpx.post(f"{API_BASE}/user.query", json={
+        "api.token": API_TOKEN,
+        "phids": [
+            task_data["authorPHID"]
+        ]
+    }).json()["0"]
+
+    if any([new_transaction in hook_transactions for new_transaction in new_transactions]):
+        httpx.post(WEBHOOK_URL, json={
+          "embeds": [
+            {
+              "title": task_data["title"],
+              "color": 14351509,
+              "url": task_data["uri"],
+              "author": {
+                "name": author_data["realName"],
+                "url": author_data["uri"],
+                "icon_url": author_data["image"]
+              },
+              "timestamp": datetime.utcnow().isoformat(),
+              "description": f"Task {task_data["objectName"]} opened with priority {task_data["priority"]}"
+            }
+          ]
+        })
 
 @app.route("/", methods=["POST"])
 def hello():
-    httpx.post(WEBHOOK_URL, json={
-        "content": str(request.get_json())
-    })
+    data = request.get_json()
+    if data.get("object", {}).get("type") == "TASK":
+        handle_task(data)
+
     return "okay"
 
 
